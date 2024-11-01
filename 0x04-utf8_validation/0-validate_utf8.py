628f6c944dcd36e54f25e34b5bf4709eaeddfcc2
astrovon <136019435+astrovon@users.noreply.github.com>
2024-11-01 01:25:29 +0100

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,38 +1,40 @@
-
-n script for UTF-8 Validation"""
+#!/usr/bin/python3
+""" UTF-8 Validation """
 
 
 def validUTF8(data):
-        """method that determines if a given data set
-            represents a valid UTF-8 encoding.
-                """
-                    numbers_bytes = 0
+    """
+    Method that determines if a given data set represents a valid
+    UTF-8 encoding.
+    """
+    number_bytes = 0
 
-                        mask_1 = 1 << 7
-                            mask_2 = 1 << 6
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
 
-                                for i in data:
+    for i in data:
 
-                                        mask_byte = 1 << 7
+        mask_byte = 1 << 7
 
-                                                if numbers_bytes == 0:
+        if number_bytes == 0:
 
-                                                            while mask_byte & i:
-                                                                            numbers_bytes += 1
-                                                                                            mask_byte = mask_byte >> 1
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
 
-                                                                                                        if numbers_bytes == 0:
-                                                                                                                        continue
-                                                                                                                        # this part
-                                                                                                                                    if numbers_bytes == 1 or numbers_bytes > 4:
-                                                                                                                                                    return False
+            if number_bytes == 0:
+                continue
 
-                                                                                                                                                            else:
-                                                                                                                                                                        if not (i & mask_1 and not (i & mask_2)):
-                                                                                                                                                                                        return False
+            if number_bytes == 1 or number_bytes > 4:
+                return False
 
-                                                                                                                                                                                                numbers_bytes -= 1
+        else:
+            if not (i & mask_1 and not (i & mask_2)):
+                    return False
 
-                                                                                                                                                                                                    if numbers_bytes == 0:
-                                                                                                                                                                                                            return True
-                                                                                                                                                                                                                return False
+        number_bytes -= 1
+
+    if number_bytes == 0:
+        return True
+
+    return False
