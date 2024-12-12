43531f41cf20b32d8f3d4f8fd557125d1a7e6297
astrovon <136019435+astrovon@users.noreply.github.com>
2024-12-12 23:21:24 +0100

    Update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,24 +1,36 @@
 #!/usr/bin/python3
+"""Interview question island perimeter"""
 
-""" Contains makeChange function"""
+def island_perimeter(grid):
+    """
+        A function that returns the perimeter of the island described in grid
 
+        grid is a list of list of integers:
 
-def makeChange(coins, total):
-        """
-            Returns: fewest number of coins needed to meet total
-                    If total is 0 or less, return 0
-                            If total cannot be met by any number of coins you have, return -1
-                                """
-                                    if not coins or coins is None:
-                                                return -1
-                                                if total <= 0:
-                                                        return 0
-                                                            change = 0
-                                                                coins = sorted(coins)[::-1]
-                                                                    for coin in coins:
-                                                                            while coin <= total:
-                                                                                        total -= coin
-                                                                                                    change += 1
-                                                                                                            if (total == 0):
-                                                                                                                        return change
-                                                                                                                            return -1
+            0 represents water
+            1 represents land
+            Each cell is square, with a side length of 1
+            Cells are connected horizontally/vertically (not diagonally).
+            grid is rectangular, with its width and height not exceeding 100
+            The grid is completely surrounded by water
+            There is only one island (or nothing).
+
+        The island doesn’t have “lakes” (water that isn’t connected to the sea).
+    """
+    perimeter = 0
+
+    if not grid:
+        return perimeter
+
+    rows, cols = len(grid), len(grid[0])
+
+    for r in range(rows):
+        for c in range(cols):
+                if grid[r][c] == 1:
+                    perimeter += 4
+                    if c > 0 and grid[r][c - 1] == 1:
+                         perimeter -= 2
+                    if r > 0 and grid[r - 1][c] == 1:
+                         perimeter -= 2
+
+    return perimeter
