8facea97a9a4f7a2417b3e493a4b05a7e8780c54
astrovon <136019435+astrovon@users.noreply.github.com>
2024-12-12 23:26:03 +0100

    Update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,36 +1,20 @@
 #!/usr/bin/python3
-"""Interview question island perimeter"""
+"""Making change interveiw"""
 
-def island_perimeter(grid):
-    """
-        A function that returns the perimeter of the island described in grid
 
-        grid is a list of list of integers:
+def makeChange(coins, total):
+    """Make total with the coins in the fewset coins possible"""
+    coins.sort(reverse=True)
+    num_coins = 0
 
-            0 represents water
-            1 represents land
-            Each cell is square, with a side length of 1
-            Cells are connected horizontally/vertically (not diagonally).
-            grid is rectangular, with its width and height not exceeding 100
-            The grid is completely surrounded by water
-            There is only one island (or nothing).
+    if total <= 0:
+        return 0
+    for coin in coins:
+        while total >= coin:
+            total -= coin
+            num_coins += 1
 
-        The island doesn’t have “lakes” (water that isn’t connected to the sea).
-    """
-    perimeter = 0
-
-    if not grid:
-        return perimeter
-
-    rows, cols = len(grid), len(grid[0])
-
-    for r in range(rows):
-        for c in range(cols):
-                if grid[r][c] == 1:
-                    perimeter += 4
-                    if c > 0 and grid[r][c - 1] == 1:
-                         perimeter -= 2
-                    if r > 0 and grid[r - 1][c] == 1:
-                         perimeter -= 2
-
-    return perimeter
+    if total == 0:
+        return num_coins
+    else:
+        return -1
