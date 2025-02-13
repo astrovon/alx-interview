bf734346750ab521f25d6da1e76737b4ba6380f4
astrovon <136019435+astrovon@users.noreply.github.com>
2025-02-13 17:30:34 +0100

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,58 +1,38 @@
 #!/usr/bin/python3
-"""Prime Game interview question"""
+"""Task 0. Prime Game module: Maria and Ben
+"""
 
 
 def isWinner(x, nums):
+    """Determines the winner of a prime game session with `x` rounds.
     """
-    where x is the number of rounds and nums is an array of n
-    Return: name of the player that won the most rounds
-    If the winner cannot be determined, return None
-    You can assume n and x will not be larger than 10000
-    You cannot import any packages in this task
-    """
-
-    if not x or not nums:
+    if x < 1 or not nums:
         return None
 
-    def is_prime(num):
-        """Checks if a number is prime"""
-        if num < 2:
-            return False
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
+    # Determine maximum value in nums to know range for prime calculation
+    max_num = max(nums)
 
-    maria_wins = 0
-    ben_wins = 0
+    # Generate prime numbers up to max_num using Sieve of Eratosthenes
+    primes = [True] * (max_num + 1)
+    primes[0] = primes[1] = False  # 0 and 1 are not prime numbers
+    for i in range(2, int(max_num**0.5) + 1):
+        if primes[i]:
+            for j in range(i*i, max_num + 1, i):
+                primes[j] = False
 
+    marias_wins, bens_wins = 0, 0
+
+    # Play x rounds of the game
     for n in nums:
-        # Initialize available numbers for this round
-        available_numbers = set(range(1, n + 1))
-        current_player = "Maria"
-
-        while available_numbers:
-            valid_choices = [num for num in available_numbers if is_prime(num)]
-            if not valid_choices:
-                break
-
-            # Current player selects the smallest prime number
-            play_number = min(valid_choices)
-            available_numbers -= set(range(play_number, n + 1, play_number))
-
-            # Switch players
-            current_player = "Ben" if current_player == "Maria" else "Maria"
-
-        # Update wins
-        if current_player == "Ben":
-            maria_wins += 1
+        prime_count = sum(primes[2:n + 1])
+        if prime_count % 2 == 1:
+            marias_wins += 1
         else:
-            ben_wins += 1
+            bens_wins += 1
 
-    # Determine overall winner
-    if maria_wins > ben_wins:
+    if marias_wins > bens_wins:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif bens_wins > marias_wins:
         return "Ben"
     else:
         return None
